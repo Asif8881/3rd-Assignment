1eb6b7ed47616964f44abc8ea8a743d294e4fc0e
Asif8881 <asif81534@gmail.com>
2025-02-01 20:49:16 +0600

    add Final Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
-    theme: {
+    theme: {  screens: {
+      xs: "480px", // Custom smaller breakpoint
+      sm: "540px", // Reduced 'sm' breakpoint
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
       extend: {},
     },
     plugins: [],
